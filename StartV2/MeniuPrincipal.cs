33709fcebe3279e97d7156c911478edf051a2a1a
gaby <gabyselejan@gmail.com>
2025-01-16 02:15:36 +0200

    adaugare MeniuPrincipal.cs

--- StartV2/MeniuPrincipal.cs
+++ StartV2/MeniuPrincipal.cs
@@ -2,16 +2,18 @@ namespace StartV2;
 
 public class MeniuPrincipal
 {
-    public void Cont(int opt)
+    public void Cont(int opt1)
     {
         Console.WriteLine("Creare cont administrator");
         Console.WriteLine("Logare administrator");
         Console.WriteLine("Creare cont utilizator");
         Console.WriteLine("Logare utilizator");
         Console.WriteLine("Alegeți o opțiune: ");
+        
+        Console.ReadLine()
     }
     
-    public void MeniuAdmin(int opt)
+    public void MeniuAdmin(int opt2)
     {
         Console.WriteLine("-----Meniu Administrator-----");
         Console.WriteLine("1. Adaugare ruta");
@@ -20,10 +22,11 @@ public class MeniuPrincipal
         Console.WriteLine("4. Gestionare trenuri");
         Console.WriteLine("5. Semnalare intarziere");
         Console.Write("Alegeți o opțiune: ");
-
+        
+        Console.ReadLine()
     }
     
-    public void MeniuUser(int opt)
+    public void MeniuUser(int opt2)
     {
         Console.WriteLine("-----Meniu Utilizator-----");
         Console.WriteLine("1. Cautare ruta");
@@ -31,6 +34,9 @@ public class MeniuPrincipal
         Console.WriteLine("3. Iesire");
         Console.Write("Alegeți o opțiune: ");
         
+        Console.ReadLine()
     }
     
+    
+
 }
