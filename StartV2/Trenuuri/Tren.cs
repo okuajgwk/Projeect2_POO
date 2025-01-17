fa9a0ed04dbd4f6b646924bc48f3be4a1d65bc35
Ivana Tincul <ivanatincul5@gmail.com>
2025-01-17 04:37:24 +0200

    incercari

--- StartV2/Trenuuri/Tren.cs
+++ StartV2/Trenuuri/Tren.cs
@@ -5,15 +5,31 @@ public abstract class Tren
     private string ID { get; set; }
     private int Number { get; set; }
     private int Capacity { get; set; }
-    private List<string> Wagons { get; set; } = new List<string>();
-    private Tren TrainType { get; set; }
+    private List<Wagon> Wagons { get; protected set; } 
+
     protected Tren(string id, int number, int capacity)
     {
         ID = id;
         Number = number;
         Capacity = capacity;
+        Wagons = new List<Wagon>();
+    }
+    
+    public void AddWagon(Wagon wagon)
+    {
+        Wagons.Add(wagon);
+    }
+    
+    private int CalculateCapacity()
+    {
+        int totalCapacity = 0;
+        foreach (var wagon in Wagons)
+        {
+            totalCapacity += wagon.SeatsNumber;
+        }
+        return totalCapacity;
     }
 
-    public abstract decimal GetPrice();
-    
+    public abstract double CalculatePrice(double basePrice);
+ 
 }
